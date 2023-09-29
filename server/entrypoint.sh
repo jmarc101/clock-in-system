c53d5df0b6e1e189641d4523b0f4cd170db8c3fc
Jean-Marc Prud'homme <jm.prudhomme@optable.co>
2023-09-29 12:51:06 -0400

    Change db to sqlite for simplicity

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
-# Wait for the database service (change "postgres" if your service is named differently)
-echo "Waiting for postgres..."
-while ! nc -z postgres 5432; do
-  sleep 0.1
-done
-echo "PostgreSQL started"
-
-# Run migrations
+# Always run migrations
 npx prisma migrate deploy
 
-# Start your server (replace with your server start command if different)
+# Start your application
 node server.js
-
