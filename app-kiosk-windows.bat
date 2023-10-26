c2f5b6d486a64eb3a23052e9dce8dfc5f3324e4b
jmarc101 <jm.prudhomme@icloud.com>
2023-10-26 09:01:07 -0700

    Scripts for kiosk-mode

--- app-kiosk-windows.bat
+++ app-kiosk-windows.bat
@@ -2,9 +2,4 @@ cd c:\repo\clock-in-system\server
 start /b node server.js
 
 cd C:\repo\clock-in-system\client
-start /b serve -s build
-
-TIMEOUT 10
-
-start msedge --kiosk http://localhost:3000
-
+start C:\Users\desktop\AppData\Roaming\npm\serve.cmd -s build
